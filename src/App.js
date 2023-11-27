095a22c1c62686f80c721278c35e618abba2bf9f
Ainun Nafiah <ainunnafiah354@gmail.com>
2023-11-27 22:37:07 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+export default function App() {
+  <>
+    <Logo />
+    <Form />
+    <PackingList />
+    <Stats />
+  </>;
+}
 
-function App() {
+function Logo() {
+  return <h1>🌴Far Away💼</h1>;
+}
+function Form() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="add-form">
+      <h3>What do you need for your 🙌trip?</h3>
     </div>
   );
 }
-
-export default App;
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+function Stats() {
+  return (
+    <footer>
+      <em>💼You have X items on your list, and you already packed X (%)</em>
+    </footer>
+  );
+}
