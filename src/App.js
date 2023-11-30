114130cb476817b5fd20bf214864a0c7de3e7686
Ainun Nafiah <ainunnafiah354@gmail.com>
2023-11-30 20:08:22 +0700

    ✅Add: Rendering the items list

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+];
+console.log(initialItems);
+
 export default function App() {
   return (
     <div className="app">
@@ -20,7 +27,26 @@ function Form() {
   );
 }
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((i) => (
+          <Item item={i} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 function Stats() {
   return (
