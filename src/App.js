8ac4f4343bd6ab88fef28dec1ff13116f9e82578
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-01-07 19:16:01 +0700

    ⭐New function: handle add items

--- src/App.js
+++ src/App.js
@@ -7,11 +7,16 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItem(item) {
+    setItems((items) => [...items, item]);
+  }
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItem} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -20,7 +25,7 @@ export default function App() {
 function Logo() {
   return <h1>🌴Far Away💼</h1>;
 }
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
 
@@ -30,8 +35,9 @@ function Form() {
     if (!description) return;
 
     const newItem = { description, quantity, packed: false, id: Math.random() };
-    console.log(newItem);
 
+    console.log(newItem);
+    onAddItems(newItem);
     setDescription("");
     setQuantity(1);
   }
@@ -55,11 +61,11 @@ function Form() {
     </form>
   );
 }
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((i) => (
+        {items.map((i) => (
           <Item propsName={i} key={i.id} />
         ))}
       </ul>
