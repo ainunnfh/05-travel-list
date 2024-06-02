3156c386a5b1165b3b1026f5a74ab64471731b31
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-06-02 20:11:43 +0700

    🐛fix (Stats): fiz for get percentange items

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,4 +1,10 @@
 export default function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list🚀</em>
+      </p>
+    );
   const itemLength = items.length;
   const itemPacked = items.filter((item) => item.packed).length;
   const percentange = Math.round((itemPacked / itemLength) * 100);
@@ -8,7 +14,7 @@ export default function Stats({ items }) {
         {percentange === 100
           ? "You got everything! Ready to go✈"
           : ` 💼You have ${itemLength} items on your list, and you already packed
-        ${itemPacked} (${percentange}%)` ? 'Start Adding some items to your packing list🚀' : ''}
+        ${itemPacked} (${percentange}%)`}
       </em>
     </footer>
   );
